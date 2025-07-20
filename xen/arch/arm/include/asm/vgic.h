7f26a95e1feea4582c416c830279a40a92dc784f
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2025-07-20 16:42:55 +0300

    xen/arm: domain_build/dom0less-build: adjust domains config to support eSPIs
    The Dom0 and DomUs logic for the dom0less configuration in create_dom0() and
    arch_create_domUs() has been updated to account for extended SPIs when
    supported by the hardware and enabled with CONFIG_GICV3_ESPI. These changes
    ensure the proper calculation of the maximum number of SPIs and eSPIs available
    to Dom0 and DomUs in dom0less setups.
    
    When eSPIs are supported by the hardware and CONFIG_GICV3_ESPI is enabled, the
    maximum number of eSPI interrupts is calculated using the ESPI_BASE_INTID
    offset (4096) and is limited to 1024, with 32 IRQs subtracted. To ensure
    compatibility with non-Dom0 domains, this adjustment is applied by the
    toolstack during domain creation, while for Dom0 or DomUs in Dom0, it is
    handled directly during VGIC initialization. If eSPIs are not supported, the
    calculation defaults to using the standard SPI range, with a maximum value of
    992 interrupt lines, as it works currently.
    
    Signed-off-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>
    
    ---
    Changes in V4:
    - consolidated the eSPI and SPI logic into a new inline function,
      vgic_def_nr_spis. Without eSPI support (either due to config being
      disabled or hardware not supporting it), it will return the regular SPI
      range, as it works currently. There are no functional changes compared
      with the previous patch version
    - removed VGIC_DEF_MAX_SPI macro, to reduce the number of ifdefs
    
    Changes in V3:
    - renamed macro VGIC_DEF_NR_ESPIS to more appropriate VGIC_DEF_MAX_SPI
    - added eSPI initialization for dom0less setups
    - fixed comment with mentions about dom0less builds
    - fixed formatting for lines with more than 80 symbols
    - updated commit message
    
    Changes in V2:
    - no changes

--- xen/arch/arm/include/asm/vgic.h
+++ xen/arch/arm/include/asm/vgic.h
@@ -347,6 +347,27 @@ extern void vgic_check_inflight_irqs_pending(struct vcpu *v,
 /* Default number of vGIC SPIs. 32 are substracted to cover local IRQs. */
 #define VGIC_DEF_NR_SPIS (min(gic_number_lines(), VGIC_MAX_IRQS) - 32)
 
+static inline unsigned int vgic_def_nr_spis(void)
+{
+#ifdef CONFIG_GICV3_ESPI
+    /*
+     * Check if the hardware supports extended SPIs (even if the appropriate
+     * config is set). If not, the common SPI range will be used. Otherwise
+     * returns the maximum eSPI INTID, supported by HW GIC, subtracted by 32.
+     * For non-Dom0 domains, the toolstack or arch_create_domUs function
+     * applies the same adjustment to cover local IRQs (please, see comment
+     * for macro that is used for regular SPIs - VGIC_DEF_NR_SPIS). We will
+     * add back this value during VGIC initialization. This ensures consistent
+     * handling for Dom0 and other domains. For the regular SPI range interrupts
+     * in this case, the maximum value of VGIC_DEF_NR_SPIS will be used.
+     */
+    if ( gic_number_espis() > 0 )
+        return ESPI_BASE_INTID + min(gic_number_espis(), 1024U) - 32;
+#endif
+
+    return VGIC_DEF_NR_SPIS;
+}
+
 extern bool vgic_is_valid_line(struct domain *d, unsigned int virq);
 
 static inline bool vgic_is_spi(struct domain *d, unsigned int virq)
