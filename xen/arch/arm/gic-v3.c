2cddf6e58715e76d91746c6861cc534b04691026
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2025-09-07 20:53:15 +0300

    Changes for V8: [01/12]: xen/arm: gicv3: refactor obtaining GIC addresses for common operations
    Changes in V8:
    - add missing breaks in switch-case (fixing 2 violations of
      MC3A2.R16.3)
    - no functional changes: moved the get_addr_by_offset() call in
      gicv3_irq_set_affinity() under appropriate check, as it should not be
      called for local IRQs
    - minor: add comments for default cases (fixing 3 violations of
      MC3A2.R16.4)
    - minor: evaluate mask to variable in gicv3_peek_irq() (fixing 2
      cautions of MC3A2.R13.2)

--- xen/arch/arm/gic-v3.c
+++ xen/arch/arm/gic-v3.c
@@ -464,8 +464,10 @@ static void __iomem *get_addr_by_offset(struct irq_desc *irqd, uint32_t offset)
         case GICD_IPRIORITYR:
             return (GICD_RDIST_SGI_BASE + GICR_IPRIORITYR0 + irqd->irq);
         default:
+            /* Invalid register offset for local IRQs */
             break;
         }
+        break;
     case NR_GIC_LOCAL_IRQS ... SPI_MAX_INTID:
         switch ( offset )
         {
@@ -483,9 +485,12 @@ static void __iomem *get_addr_by_offset(struct irq_desc *irqd, uint32_t offset)
         case GICD_IPRIORITYR:
             return (GICD + GICD_IPRIORITYR + irqd->irq);
         default:
+            /* Invalid register offset for SPIs */
             break;
         }
+        break;
     default:
+        /* Invalid INTID */
         break;
     }
 
@@ -513,12 +518,13 @@ static void gicv3_poke_irq(struct irq_desc *irqd, u32 offset, bool wait_for_rwp)
 
 static bool gicv3_peek_irq(struct irq_desc *irqd, u32 offset)
 {
+    uint32_t mask = 1U << (irqd->irq % 32);
     void __iomem *addr = get_addr_by_offset(irqd, offset);
 
     if ( addr == NULL )
         return false;
 
-    return !!(readl(addr) & (1U << (irqd->irq % 32)));
+    return !!(readl(addr) & mask);
 }
 
 static void gicv3_unmask_irq(struct irq_desc *irqd)
@@ -1315,8 +1321,12 @@ static void gicv3_irq_set_affinity(struct irq_desc *desc, const cpumask_t *mask)
 {
     unsigned int cpu;
     uint64_t affinity;
-    void __iomem *addr = get_addr_by_offset(desc, GICD_IROUTER);
+    void __iomem *addr;
 
+    if ( desc->irq < NR_GIC_LOCAL_IRQS )
+        return;
+
+    addr = get_addr_by_offset(desc, GICD_IROUTER);
     if ( addr == NULL )
         return;
 
@@ -1328,9 +1338,7 @@ static void gicv3_irq_set_affinity(struct irq_desc *desc, const cpumask_t *mask)
     affinity = gicv3_mpidr_to_affinity(cpu);
     /* Make sure we don't broadcast the interrupt */
     affinity &= ~GICD_IROUTER_SPI_MODE_ANY;
-
-    if ( desc->irq >= NR_GIC_LOCAL_IRQS )
-        writeq_relaxed_non_atomic(affinity, addr);
+    writeq_relaxed_non_atomic(affinity, addr);
 
     spin_unlock(&gicv3.lock);
 }
