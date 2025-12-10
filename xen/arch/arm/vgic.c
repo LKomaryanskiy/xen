1994c3eb02b8666becefe0b387bf03589b28f55b
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2025-12-11 00:15:33 +0200

    Revert "xen/arm: Add support for GSX per-OSID irq counters"
    This reverts commit d4192ca563d80ca775eadbce278eccc3ebc1bc8e.
    
    The code is Gen3-specific, where only a single SPI IRQ is available for
    all VMs. It includes hardcoded addresses that are mapped to Xen, but
    Gen5 has a different device (SMMU pere for eMMC) in this range, so it
    may lead to potential issues. Moreover, we do not need to change IRQ
    injection to the domains, as on Gen5, there are 8 dedicated IRQs for
    each VM.
    
    Signed-off-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>
    Acked-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- xen/arch/arm/vgic.c
+++ xen/arch/arm/vgic.c
@@ -335,9 +335,8 @@ void register_vgic_ops(struct domain *d, const struct vgic_ops *ops)
    d->arch.vgic.handler = ops;
 }
 
-extern bool opt_rcar3_gsx;
 extern const int gsx_irq_num;
-extern void remove_gsx_domain(struct domain *d);
+extern void remove_gsx_guest(struct domain *d);
 
 void domain_vgic_free(struct domain *d)
 {
@@ -351,8 +350,8 @@ void domain_vgic_free(struct domain *d)
 
         p = spi_to_pending(d, virq);
 
-        if ( opt_rcar3_gsx && p->irq == gsx_irq_num )
-            remove_gsx_domain(d);
+        if ( p->irq == gsx_irq_num )
+            remove_gsx_guest(d);
 
         if ( p->desc )
         {
